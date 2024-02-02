62c8bbd1ba162aed8b38be85b415677274aa7013
segleston <suraheg@gmail.com>
2024-02-02 15:40:56 +0000

    Added table of contents

--- index.js
+++ index.js
@@ -50,23 +50,40 @@ const questions = [
         type: 'input',
         name: 'email',
         message: "What is your email address?"
-    },
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    `# UK-VIRT-FE-PT-11-2023-U-LOLC
-    // Project title
+    `# ${data.title}
     ## Description 
-    ## Table of Contents: //Links linking to the different areas in the readme
-    ## Installation
-## Usage
-//List of options - adds badge to top and details underneath
-## License
-## Test and Deploy
-## Contributing
-## Questions //ask user for github username, then link to github goes here. Also email address with mailto link`
 
+    ## Table of Contents: //Links linking to the different areas in the readme
+    - [Description](#Description)
+    - [Installation Instructions](#Installation Instructions)
+    - [Usage Information](#Usage Information) 
+    - [Contributing Guidelines](#Contributing)
+    - [Test Instructions](#Test and Deploy) 
+    - [Questions & Contact Info](#Questions)
+    - [License](#License)
+
+## Installation Instructions 
+
+## Usage Information 
+//List of options - adds badge to top and details underneath
+
+## Test and Deploy
+
+## Contributing
+
+## Questions 
+
+- [GitHub](https://www.github.com/${data.github})
+- If you have any further questions, please don't hesitate to [email me](mailto:${data.email})
+
+## License`
+
+`[Bootstrap](https://getbootstrap.com/docs/5.3/getting-started/introduction/)`
 }
 
 // function to initialize program

efb8359365cc32d1229b379d97e46573d69e548b
segleston <suraheg@gmail.com>
2024-02-02 12:23:53 +0000

    Added starter code and started on readme text

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    `# UK-VIRT-FE-PT-11-2023-U-LOLC
+    // Project title
+    ## Description 
+    ## Table of Contents: //Links linking to the different areas in the readme
+    ## Installation
+## Usage
+//List of options - adds badge to top and details underneath
+## License
+## Test and Deploy
+## Contributing
+## Questions //ask user for github username, then link to github goes here. Also email address with mailto link`
+
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
